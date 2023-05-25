b7afd267b978730ea79ff3863bb4dde4536cd75d
martinapiras <131855775+martinapiras@users.noreply.github.com>
2023-05-25 19:06:01 +0200

    Update script.js
    fixed hero carousel

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ import {
 } from "./fn.js";
 
 // HERO //
-export const heroImages = [
+const heroImages = [
   {
     id: 1,
     alt: "hero1",
@@ -29,80 +29,40 @@ export const heroImages = [
     src: "https://picsum.photos/1200/800?4",
   },
 ];
-export const heroEl = document.querySelector(".hero");
-export const heroContentEl = document.createElement("div");
-export const heroTitleEl = document.createElement("h1");
-export const heroButtonEl = document.createElement("button");
-
-// HERO CAROUSEL //
-export const allImages = document.querySelectorAll(".heroImage");
-export let currentImage = 0;
-export let totalImages = allImages.length - 1;
+const heroEl = document.querySelector(".hero");
+const heroContentEl = document.createElement("div");
+const heroTitleEl = document.createElement("h1");
+const heroButtonEl = document.createElement("button");
 
 // PRODUCTS //
 export const productsEl = document.querySelector(".products");
-export const modalEl = document.querySelector(".modal");
-export let productsObj = [];
+const modalEl = document.querySelector(".modal");
+let productsObj = [];
 
 // PRODUCTS SEARCH //
-export let userSearch = "";
-export const searchbarEl = document.querySelector(".search");
+let userSearch = "";
+const searchbarEl = document.querySelector(".search");
 
 // FILTERS //
 // CATEGORY
-export const categoryFilter = document.querySelector(".category");
-export let categoryChoice = "";
+const categoryFilter = document.querySelector(".category");
+let categoryChoice = "";
 // PRICE
-export const priceFilter = document.querySelector(".price");
-export let priceChoice = "";
+const priceFilter = document.querySelector(".price");
+let priceChoice = "";
 // RATING
-export const ratingFilter = document.querySelector(".rating");
-export let ratingChoice = "";
+const ratingFilter = document.querySelector(".rating");
+let ratingChoice = "";
 
 // CART //
 export let cartItems = [];
 export const cartBtnEl = document.querySelector(".cartBtn");
-export const cartAfterEl = document.querySelector(".cart:after");
+const cartAfterEl = document.querySelector(".cart:after");
 export const cartModalWrapperEl = document.querySelector(".cartModalWrapper");
 
 // HAMBURGER MENU //
-export const hamburgerMenuIconEl = document.querySelector(".hamburgerMenu");
-export const hamburgerMenuEl = document.querySelector(
-  ".hamburgerMenu__contents"
-);
-
-// BRANDS //
-export const brands = [
-  {
-    id: 1,
-    alt: "brand1",
-    src: ".assets/brands/Logo-Logo.svg",
-    height: 50,
-    width: 100,
-  },
-  {
-    id: 2,
-    alt: "brand2",
-    src: ".assets/brands/Logo-Logo-Black-with-White-Background.svg",
-    height: 50,
-    width: 100,
-  },
-  {
-    id: 3,
-    alt: "brand3",
-    src: ".assets/brands/Logo-Logo-Grayscale.svg",
-    height: 50,
-    width: 100,
-  },
-  {
-    id: 4,
-    alt: "brand4",
-    src: ".assets/brands/Logo-Logo-White-with-Black-Background.svg",
-    height: 50,
-    width: 100,
-  },
-];
-export const brandsEl = document.querySelector(".brands");
+const hamburgerMenuIconEl = document.querySelector(".hamburgerMenu");
+const hamburgerMenuEl = document.querySelector(".hamburgerMenu__contents");
 
 //////////////////////////////////////////
 
@@ -118,6 +78,12 @@ heroButtonEl.textContent = "Shop Now →";
 heroContentEl.append(heroTitleEl, heroButtonEl);
 heroEl.append(heroContentEl);
 heroImages.forEach((image) => heroEl.append(heroImgGen(image)));
+
+// HERO CAROUSEL //
+export const allImages = document.querySelectorAll(".heroImage");
+export let currentImage = 0;
+export let totalImages = allImages.length - 1;
+
 document.querySelector(".body").append(createLogin());
 
 //////////////////////////////////////////
